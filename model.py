b2b9f242a30206098f4e67d5cf863fcb15562ae5
Milan Tancak <tancak.milan@gmail.com>
2022-02-13 19:56:35 +0000

    refactored the model class and corrected the skip connection and relu logic, prints out a summary of the model

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torchsummary import summary
 
 
 def conv0(in_features, out_features = 64):
@@ -17,7 +18,7 @@ def conv0(in_features, out_features = 64):
     ])
 
 
-def conv_x(in_features, mid_features, out_features):
+def conv_x(in_features, mid_features, out_features, final_stride=1):
     return nn.ModuleList([
             nn.Conv2d(
                 in_channels=in_features,
@@ -39,19 +40,19 @@ def conv_x(in_features, mid_features, out_features):
             nn.Conv2d(in_channels=mid_features,
                   out_channels=out_features,
                   kernel_size=1,
-                  stride=1,
+                  stride=final_stride,
                   padding=0,
                   bias=False),
             nn.BatchNorm2d(out_features)
         ])
 
 
-def skip_x(in_features, out_features):
+def skip_x(in_features, out_features, final_stride=1):
     return nn.ModuleList([
             nn.Conv2d(in_channels=in_features,
                   out_channels=out_features,
                   kernel_size=1,
-                  stride=1,
+                  stride=final_stride,
                   padding=0,
                   bias=False),
             nn.BatchNorm2d(out_features)
@@ -59,24 +60,41 @@ def skip_x(in_features, out_features):
 
 
 class ResNet50(nn.Module):
-    def __init__(self, num_channels=3):
+    def __init__(self, num_channels=3, classes=10):
         super(ResNet50, self).__init__()
 
-        self.blocks = nn.ModuleList()
-        self.skip_layers = nn.ModuleList()
+        self.num_channels = num_channels
+        self.classes = classes
+
         block_repeats_lis = [3, 4, 6, 3]
         in_features_lis = [64, 256, 512, 1024]
         mid_features_lis = [64, 128, 256, 512]
         scale = 4
+
+        self.blocks = nn.ModuleList()
+        self.skip_layers = nn.ModuleList()
+        self.relu_layers = nn.ModuleList()
+        self.classifier_block = nn.ModuleList()
 
         self.conv0_block = conv0(num_channels)
 
         for repeat, in_features, mid_features in zip(block_repeats_lis, in_features_lis, mid_features_lis):
             self.blocks.append(conv_x(in_features, mid_features, scale*mid_features))
             self.skip_layers.append(skip_x(in_features, scale*mid_features))
-            for i in range(repeat):
-                self.blocks.append(conv_x(scale*mid_features, mid_features, scale*mid_features))
+            self.relu_layers.append(nn.ReLU(inplace=True))
+            for i in range(repeat-2):
+                self.blocks.append(conv_x(scale*mid_features, mid_features, scale*mid_features, final_stride=1))
                 self.skip_layers.append(nn.ModuleList([nn.Identity()]))
+                self.relu_layers.append(nn.ReLU(inplace=True))
+            self.blocks.append(conv_x(scale*mid_features, mid_features, scale*mid_features, final_stride=2))
+            self.skip_layers.append(skip_x(mid_features*scale, mid_features*scale, final_stride=2))
+            self.relu_layers.append(nn.ReLU(inplace=True))
+
+        self.latent_representation_size = int(2048 * 4 * 4)
+
+        self.classifier_block.append(nn.Linear(self.latent_representation_size, 1000))
+        self.classifier_block.append(nn.Linear(1000, self.classes))
+        self.classifier_block.append(nn.Softmax(dim=1))
 
     def forward(self, x):
         x_ = x
@@ -84,16 +102,11 @@ class ResNet50(nn.Module):
         for module in self.conv0_block:
             x_ = module(x_)
 
-        print("num layers = " + str(len(self.blocks)))
-        for block, skip_layer in zip(self.blocks, self.skip_layers):
-            print("layer, x_.shape = " + str(x_.shape))
-
+        for index, (block, skip_layer, relu) in enumerate(zip(self.blocks, self.skip_layers, self.relu_layers)):
             interm_x_ = x_.clone()
 
             for module in block:
-                print("module")
                 interm_x_ = module(interm_x_)
-                print(" shape = " + str(interm_x_.shape))
 
             skip_x_ = x_.clone()
 
@@ -102,9 +115,17 @@ class ResNet50(nn.Module):
 
             x_ = interm_x_ + skip_x_
 
+            x_ = relu(x_)
+
+        x_ = x_.reshape((x_.shape[0], torch.prod(torch.tensor(x_.shape)[1:])))
+
+        for layer in self.classifier_block:
+            x_ = layer(x_)
+
         return x_
 
 
 if __name__ == "__main__":
-    model = ResNet50(num_channels=3)
-    model(torch.rand((1, 3, 224, 224)))
+    model = ResNet50(num_channels=3).to("cuda")
+    model(torch.rand((1, 3, 224, 224)).to("cuda"))
+    summary(model, (3, 224, 224))
